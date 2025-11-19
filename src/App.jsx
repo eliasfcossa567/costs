90b08aec452c0632c2987834a9845a89d1e2d83a
eliascossajr <eliasfcossajr@gmail.com>
2025-11-19 19:16:21 +0000

    Estrutura inicial da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import Home from "./components/pages/Home"
+import Company from "./components/pages/Company.jsx"
+import Contact from "./components/pages/Contact.jsx"
+import NewProject from "./components/pages/NewProject.jsx"
 function App() {
 
   return (
-    <>
-     <p>Costs</p>
-    </>
+    <Router>
+      <ul>
+        <Link to="/">Início</Link> |{" "}
+        <Link to="/company">Empresa</Link> |{" "}
+        <Link to="/contact">Contacto</Link> |{" "}
+        <Link to="/newproject">Novo Projecto</Link>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      <p>Footer</p>
+    </Router>
   )
 }
 
